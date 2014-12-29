01b71ff9e6702ff575e892a78672bbad11d59d4f
Marco D <marco.casanova@icogroup.com>
2014-12-29 08:22:08 -0430

    Primer commit, trabajando en el Menu y en la primera vissta account-overview

---
+++ views/account_overview.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lucky</title>
+
+    <!-- Bootstrap -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/account_overview.css" rel="stylesheet">
+     <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap theme -->
+    <link href="../css/bootstrap-theme.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+<div class="container-fluid" style="margin-top: 99px;">
+  <?php include '../template/navbar.template.html';?>
+</div>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-2">
+      <div class="row">
+        <div class="col-md-12">
+          <p class="font_arial">MY ACCOUNT</p>
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-10">My Bets</div>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-10">My Casino</div>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-10">My Poker</div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-12">
+          <p class="font_arial">ACCOUNT MOVEMENTS</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-10">My Bets</div>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-10">My Casino</div>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-10">My Poker</div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-12">
+          <p class="font_arial">ACCOUNT MOVEMENTS</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-10">My Bets</div>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-10">My Casino</div>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-10">My Poker</div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-12">
+          <p class="font_arial">ACCOUNT MOVEMENTS</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-10">My Bets</div>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-10">My Casino</div>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-10">My Poker</div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <p class="font_arial">ACCOUNT MOVEMENTS</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-10">My Bets</div>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-10">My Casino</div>
+      </div>
+      <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-10">My Poker</div>
+      </div>
+    </div>
+
+    
+
+
+  <div class="col-md-6">
+    <div class="row">
+        <div class="col-md-12">
+          <p class="font_arial">ACCOUNT MOVEMENTS</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <p class="font_arial"><hr></p>
+        </div>
+      </div>
+  </div>
+  <div class="col-md-4">.col-md-4</div>
+</div>
+
+      <!-- Main component for a primary marketing message or call to action -->
+      <div class="jumbotron" >
+        <h1>Navbar example</h1>
+        <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+        <p>To see the difference between static and fixed top navbars, just scroll.</p>
+        <p>
+          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
+        </p>
+      </div>
+
+    </div> <!-- /container -->
+
+
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../js/bootstrap.min.js"></script>
+  </body>
+</html>
