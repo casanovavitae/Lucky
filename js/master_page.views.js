fdefdf03119756696a390d0f08fc4ca62589615c
Marco D <marco.casanova@icogroup.com>
2015-01-16 16:39:02 -0430

    Subida todas las plantillas

--- js/master_page.views.js
+++ js/master_page.views.js
@@ -1,9 +1,14 @@
+
+
+
 $(document).ready(function(){
 
 $("#menu_top").load("../template/navbar.template.html");
 $("#menu_top-a").load("../template/navbar.template-a.html");
 $("#menu_left").load("../template/navbar_left.template.html");
 $("#menu_left-a").load("../template/navbar_left.template-a.html");
+$("#register").load("../template/register.template.html");
+
 $("#foot").load("../template/footer.template.html");
 
 $(document.body).on('click', '.inactivo' ,function(){
@@ -13,10 +18,29 @@ $(document.body).on('click', '.inactivo' ,function(){
   $( this ).removeClass("inactivo").addClass("activo");
 });
 
+
+$(document.body).on('click', '.bg-verde-dark' ,function(){
+	$( ".bg-verde-dark" ).each(function( index ) {
+    	$( this ).removeClass("activo").addClass("inactivo");
+    });
+  $( this ).removeClass("inactivo").addClass("activo");
+
+
+  
+
+
+});
+
+
 $(document).ready( function() {
+
+
     $('#myCarousel').carousel({
-		interval:   4000
+		interval:   4000 //Para activar el movimiento automatico del slides
+		//interval:   false //Para desactivar el movimiento automatico del slides
 	});
+
+
 	
 	var clickEvent = false;
 	$('#myCarousel').on('click', '.nav a', function() {
